58901a5d6d79701a1b733d6891dc610f01eea600
Uditha Adhikary <chathurangaadhikary@gmail.com>
2023-10-24 22:07:54 +0530

    backend

---
+++ fullstack-crudBackend/src/main/java/com/crudBkaned/fullstackcrudBackend/FullstackCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crudBkaned.fullstackcrudBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackCrudBackendApplication.class, args);
+	}
+
+}
---
+++ fullstack-crudBackend/src/test/java/com/crudBkaned/fullstackcrudBackend/FullstackCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudBkaned.fullstackcrudBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
