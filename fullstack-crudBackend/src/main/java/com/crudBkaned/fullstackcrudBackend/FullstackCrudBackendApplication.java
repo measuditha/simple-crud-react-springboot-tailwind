15ab6a2595ee56a6c644e0935100fcc173bced2b
Uditha Adhikary <chathurangaadhikary@gmail.com>
2023-10-28 10:36:32 +0530

    backend

--- fullstack-crudBackend/src/main/java/com/crudBkaned/fullstackcrudBackend/FullstackCrudBackendApplication.java
+++ fullstack-crudBackend/src/main/java/com/crudBkaned/fullstackcrudBackend/FullstackCrudBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FullstackCrudBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FullstackCrudBackendApplication.class, args);
 	}
 
